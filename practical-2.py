5c3a2bdf913fb831a8154f8d9d4e44712007a86e
AnugrahGupta <112945888+AnugrahGupta@users.noreply.github.com>
2022-09-29 15:09:45 +0100

    Add files via upload

---
+++ practical-2.py
@@ -0,0 +1,8 @@
+from sys import argv
+a= float(argv[1])
+b= float(argv[2])
+print("{:f} + {:f} = {:f}".format(a, b, a+b))
+print("{:f} - {:f} = {:f}".format(a, b, a-b))
+print("{:f} * {:f} = {:f}".format(a, b, a*b))
+print("{:f} / {:f} = {:f}".format(a, b, a/b))
+print("{:f} ** {:f} = {:f}".format(a, b, a**b))
